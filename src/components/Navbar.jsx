6acecd0cb09b43d65c4223490f1b27729bb30e52
@niranjansharma9057 <niranjansharma9057@gmail.com>
2024-09-07 15:07:26 +0530

    hgfd

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,60 +1,88 @@
-import React, { useState } from 'react';
-import { Container, Row, Col, Navbar, Nav, Button } from 'react-bootstrap';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFacebookF, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons';
-import { faBars } from '@fortawesome/free-solid-svg-icons'; // Correct import for faBars
 import './Navbar.css'; 
 import logo from "../images/jslee.png";
+import React, { useState } from 'react';
+import { Navbar, Nav, Button } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons';
 import { Link } from 'react-router-dom';
 
 const CustomNavbar = () => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggle = () => {
-    setIsOpen(!isOpen);
-    console.log(isOpen)
+  const [activeLink, setActiveLink] = useState("/");
+  const handleNavClick = (path) => {
+    setActiveLink(path);
   };
-
   return (
-    <Navbar expand="lg" className="custom-navbar">
-      <Container>
-        <Row className="w-100 align-items-center">
-          {/* Logo Column */}
-          <Col xs={6} md={2}>
-            <Navbar.Brand as={Link} to="/">
-              <img
-                src={logo}
-                width="90"
-                height="40"
-                className="d-inline-block align-top"
-                alt="Logo"
-              />
-            </Navbar.Brand>
-          </Col>
+    <Navbar expand="lg" className="navbar-custom fixed-top">
+      <div className="container d-flex justify-content-between align-items-center">
 
-          {/* Navigation Links Column */}
-          <Col xs={12} md={7} className={isOpen ? "mob-nav": "nav-Menu"}>
-            <Nav className="justify-content-center">
-              <Nav.Link as={Link} to="/" className="nav-link-custom mx-2">Home</Nav.Link>
-              <Nav.Link as={Link} to="/aboutUs" className="nav-link-custom mx-2">About Us</Nav.Link>
-              <Nav.Link as={Link} to="/projects" className="nav-link-custom mx-2">Our Projects</Nav.Link>
-              <Nav.Link as={Link} to="/services" className="nav-link-custom mx-2">Services</Nav.Link>
-              <Nav.Link as={Link} to="/contact" className="nav-link-custom mx-2">Contact Us</Nav.Link>
-            </Nav>
-          </Col>
-
-          {/* Icons and Burger Icon Column */}
-          <Col xs={6} md={3} className="d-flex justify-content-end align-items-center icons-burger">
-            <Button variant="warning" className="mx-2 nav-button">Enquire Us</Button>
-            <FontAwesomeIcon icon={faFacebookF} className="mx-2 social-icon" />
-            <FontAwesomeIcon icon={faTwitter} className="mx-2 social-icon" />
-            <FontAwesomeIcon icon={faInstagram} className="social-icon" />
-            <FontAwesomeIcon onClick={toggle} icon={faBars} className="burger-icon" />
-          </Col>
-        </Row>
-        </Container>
+        <Navbar.Brand as={Link} to="/" className="d-flex align-items-center">
+          <img
+            src={logo}
+            alt="Company Logo"
+            className="logo"
+            style={{ width: '100px' }}
+          />
+        </Navbar.Brand>
+        <Navbar.Collapse id="navbar-nav" className="justify-content-center">
+          <Nav className="mr-auto">
+            <Nav.Link
+              as={Link}
+              to="/"
+              onClick={() => handleNavClick('/')}
+              className={activeLink === '/' ? 'active-navlink' : ''}
+            >
+              Home
+            </Nav.Link>
+            <Nav.Link
+              as={Link}
+              to="/aboutUs"
+              onClick={() => handleNavClick('/aboutUs')}
+              className={activeLink === '/aboutUs' ? 'active-navlink' : ''}
+            >
+              About Us
+            </Nav.Link>
+            <Nav.Link
+              as={Link}
+              to="/projects"
+              onClick={() => handleNavClick('/projects')}
+              className={activeLink === '/projects' ? 'active-navlink' : ''}
+            >
+              Our Projects
+            </Nav.Link>
+            <Nav.Link
+              as={Link}
+              to="/services"
+              onClick={() => handleNavClick('/services')}
+              className={activeLink === '/services' ? 'active-navlink' : ''}
+            >
+              Services
+            </Nav.Link>
+            <Nav.Link
+              as={Link}
+              to="/contact"
+              onClick={() => handleNavClick('/contact')}
+              className={activeLink === '/contact' ? 'active-navlink' : ''}
+            >
+              Contact Us
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+        <div className="d-flex align-items-center social-and-toggle">
+          <Button variant="warning" className="bt_nav">Enquire</Button>
+          <a href="https://facebook.com" className="ms-3 social-icon">
+            <FontAwesomeIcon icon={faFacebook} />
+          </a>
+          <a href="https://twitter.com" className="ms-3 social-icon">
+            <FontAwesomeIcon icon={faTwitter}  />
+          </a>
+          <a href="https://instagram.com" className="ms-3 social-icon">
+            <FontAwesomeIcon icon={faInstagram} />
+          </a>
+          <Navbar.Toggle size='md' className='ms-2' aria-controls="navbar-nav" />
+        </div>
+      </div>
     </Navbar>
   );
-}
+};
 
 export default CustomNavbar;
